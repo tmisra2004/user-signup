692153b47083f7411f44bce56bf23742e1764d82
Tavish Misra <tmisra2004@gmail.com>
2017-10-04 11:46:28 -0500

    All done and functioning except for persistence in username and e-mail fields after submit

--- main.py
+++ main.py
@@ -19,30 +19,49 @@ def user_signup():
     password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
     
     if username == "":
-        error = "Please enter a username."
-        return error
+        username_error = "Please enter a username."
+    
+    if len(username) <= 3 or len(username) > 20:
+        username_error = "Username must be at least 3 characters but not more than 20 characters."
+        
 
     if password == "":
-        error = "Please enter a password."
-        return error
+        password_error = "Please enter a password."
 
-    if len(username) < 3 or len(username) > 20:
-        error = "Username must be at least 3 characters but not more than 20 characters."
-        return error
+    if len(password) < 3 or len(password) > 20:
+        password_error = "Password must be at least 3 characters but no more than 20 characters long."
 
-    if password != verify:
-        error = "Passwords do not match."
-        return error
-
-    test = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
-
-    if test == None:
-        error = "Please enter a valid email address."
-        return error
+    if " " in password:
+        password_error = "Password cannot contain spaces."
     
-    else:
+    if verify == "" or verify != password:
+        verify_error = "Passwords do not match."
+        
+
+    if email != "":
+        
+        if not re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email):
+            email_error = "Please enter a valid email address."
+       
+    
+    if not username_error and not password_error and not verify_error and not email_error:
         return render_template("success.html", username = username)
+
+    else:
+        return render_template(
+            "index.html",
+            username_error = username_error,
+            password_error = password_error,
+            verify_error = verify_error,
+            email = email,
+            email_error = email_error
+        )
 
 app.run()
